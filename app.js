6acec3a587ebef4b9cbdbe92862fd1c520eac5cc
Ravi <prakas.ravi.91@gmail.com>
2020-08-20 19:45:57 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,98 @@
 const express = require('express');
 const app = express();
+const pageRoutes = express.Router();
+const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
-// let Test = require('./test.model');
-var port = process.env.port || 3000;
-app.get('/',(req, res)=> {
-    res.send('Hello World Second Branch');
-});
+const cors = require('cors');
+
+const PORT = process.env.PORT || 4000;
+
+let Test = require('./test.model');
+
+
+app.use(cors());
+app.use(bodyparser.json());
+
 mongoose.connect('mongodb://127.0.0.1:27017/testdb', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', function(){
     console.log('MongoDB Connection Established Successfully');
 });
-app.listen(port, ()=> {
-    console.log('Server running on port:' + port);
+
+pageRoutes.route('/').get(function(req, res){
+    Test.find(function(error, test){
+        if(error){
+            console.log('Error in home page');
+        }
+        else{
+            res.json(test);
+        }
+    });
+});
+
+pageRoutes.route('/:id').get(function(req, res){
+    let id = req.params.id;
+    Test.findById(id, function(error, test){
+        if(error){
+            console.log('Error in get data page');
+        }
+        else{
+            res.json(test);
+        }
+    });
+});
+
+pageRoutes.route('/add').post(function(req, res){
+    // var testValue = req.json({'emp_ID': '1', 'emp_Name': 'Ravi', 'emp_status': true});
+    // let test = new Test(testValue);
+    let test = new Test(req.body);
+    test.save()
+        .then(test => {
+            res.status(200).json({'result': 'item added successfully2'});
+        })
+        .catch(error => {
+            res.status(400).send('Adding item failed');
+        });
+});
+
+pageRoutes.route('/update/:id').post(function(req, res){
+    let id = req.params.id;
+    Test.findById(id, function(error, test){
+        if(!test){
+            res.status(400).send('Updating item failed');
+        }
+        else{
+            test.emp_ID = req.body.emp_ID;
+            test.emp_Name = req.body.emp_Name;
+            test.emp_status = req.body.emp_status;
+
+            test.save().then(test => {
+                res.status(200).json('Item Updated');
+            })
+            .catch(error => {
+                res.status(400).send('Updating item failed, while updating');
+            })
+        }
+    });
+});
+
+pageRoutes.route('/delete/:id').delete(function(req,res){
+    let id = req.params.id;
+    Test.findById(id, function(error, test){
+        if(test){
+            test.deleteOne().then(test => {
+                res.status(200).json('Item Deleted');
+            })
+            .catch(error => {
+                res.status(400).send('request failed while deleting the item');
+            });
+        }
+    });
+});
+
+app.use('/home', pageRoutes);
+
+app.listen(PORT, function(){
+    console.log('Working');
 });
